9422ad3fca56f9a2537e67cf18d9ae0b19fbd319
danielcuca <elzinhocuca@yahoo.com>
2023-10-13 22:44:54 -0300

    middleware de email e senha no login feitos

--- app/backend/src/middlewares/login.middleware.ts
+++ app/backend/src/middlewares/login.middleware.ts
@@ -2,9 +2,18 @@ import { Request, Response, NextFunction } from 'express';
 
 function loginMiddleware(req: Request, res: Response, next: NextFunction) {
   const { email, password } = req.body;
+  const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,}$/;
 
   if (!email || !password) {
     return res.status(400).json({ message: 'All fields must be filled' });
+  }
+
+  if (!emailRegex.test(email)) {
+    return res.status(401).json({ message: 'Invalid email or password' });
+  }
+
+  if (password.length < 6) {
+    return res.status(401).json({ message: 'Invalid email or password' });
   }
 
   next();
